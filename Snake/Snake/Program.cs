6480f6416954116ca832d0745f361d331f81d487
ElenaUtkina <bovina.elena@yandex.ru>
2016-07-24 21:41:47 +0700

    Add class Snake

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -10,12 +10,6 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            Point p1 = new Point(1, 3, '*');
-            p1.Draw();
-
-            Point p2 = new Point(4, 5, '#');
-            p2.Draw();
-
             Console.SetBufferSize(80, 25);
 
             HorizontalLine upline = new HorizontalLine(0, 78, 0, '*');
@@ -26,6 +20,11 @@ namespace Snake
             downline.Draw();
             leftline.Draw();
             rightline.Draw();
+
+
+            Point p = new Point(4, 5, '*');
+            Snake snake = new Snake(p, 4, Direction.RIGHT);
+            snake.Draw();
 
             Console.ReadLine();
         }
